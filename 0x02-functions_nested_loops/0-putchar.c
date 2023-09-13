013ab0b4532562066790451a47aaea492ad28925
MrCode <mubashrahmed.ma@gmail.com>
2023-09-13 13:53:56 -0700

    0-

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,18 @@
-#include <unistd.h>
-
+#include "main.h"
 /**
- * main - Entry point
- *
- * Description: Prints "_putchar" followed by a new line
- *
- * Return: Always 0 (Success)
+ * main - prints _putchar, fllowd by a new linees
+ * Return: Alwys 0 (Success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "_putchar\n", 9);
+	char abe[] = "_putchar";
+
+	int c;
+
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(abe[c]);
+	}
+	_putchar('\n');
 	return (0);
 }
