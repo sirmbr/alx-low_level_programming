fccb788effad4a9677a278833f9a39a01e18c285
sirmbr <mubashrahmed.ma@gmail.com>
2023-10-20 19:08:54 -0700

    0x12

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		n++;
+	}
+
+	return (n);
+}
